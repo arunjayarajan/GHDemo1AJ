e790957594f7199bebd0859f1150c199c93351a7
arun.jayarajan <L00171224@atu.ie>
2022-09-19 10:40:07 +0100

    Added FirstProgram.java

---
+++ code/FirstProgram.java
@@ -0,0 +1,6 @@
+public class HelloLYIT {
+ public static void main(String[] args) {
+ // Prints "Hello, LYIT from ARUN JAYARAJAN" as output
+ System.out.println("Hello, LYIT from ARUN JAYARAJAN");
+ }
+}
